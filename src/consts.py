261d509d1d052e5cc98bd0299cffb45993d626c7
gkstd <ildar@.ru>
2026-03-10 17:59:01 +0300

    Документирвоание кода

--- src/consts.py
+++ src/consts.py
@@ -1,26 +1,29 @@
+"""Модуль констант"""
 class FileType:
+    """Поддерживаемые типы файлов"""
     IMAGE: str = "image"
     VIDEO: str = "video"
     AUDIO: str = "audio"
     DOCUMENT: str = "document"
     ARCHIVE: str = "archive"
-    EXECUTABLE: str = "executable"
+    EXECUTABLE: str = "executable" # Исполняемые
     TORRENT: str = "torrent"
     PRESENTATION: str = "presentation"
-    SPREADSHEET: str = " spreadsheet"
+    SPREADSHEET: str = " spreadsheet" # Таблицы
     SOURCE_CODE: str = "source_code"
     OTHER: str = "other"
 
 
+# Карта типов файлов и списка расширений
 FILE_TYPE_EXTENSIONS_MAPPING: dict[str, tuple[str, ...]] = {
     FileType.IMAGE: ("jpg", "jpeg", "png", "gif", "bmp", "svd"),
     FileType.VIDEO: ("mp4", "avi", "mov", "mkv", "wmv", "mpeg"),
-    FileType.AUDIO: ("mp3", "wav", "flac","aac", "ogg"),
+    FileType.AUDIO: ("mp3", "wav", "flac", "aac", "ogg"),
     FileType.DOCUMENT: ("pdf", "doc", "docx", "txt", "rtf", "odt"),
     FileType.ARCHIVE: ("zip", "rer", "7z", "tar", "gz"),
     FileType.EXECUTABLE: ("exe", "msi", "bat", "cmd"),
     FileType.TORRENT: ("torrent", "magnet"),
-    FileType.PRESENTATION: ("ppt", "pptx","odp"),
+    FileType.PRESENTATION: ("ppt", "pptx", "odp"),
     FileType.SPREADSHEET: ("xls", "xlsx", "xlsm", "ods", "csv"),
-    FileType.SOURCE_CODE: ("js", "html", "py", "ts", "css")
+    FileType.SOURCE_CODE: ("js", "html", "py", "ts", "css"),
 }
--- src/models/path.py
+++ src/models/path.py
@@ -1,9 +1,24 @@
+"""Модуль модели для работы с путем каталога"""
 from pathlib import Path
-from typing import Generator, Any
+from typing import Any, Generator
 
 
 class PathModel:
+    """
+    Модель пути каталога
+
+    Attributes:
+        _path (Path): объект Path с путем каталога
+
+    """
     def __init__(self, path: Path, skip_check: bool = False) -> None:
+        """
+        Инициализация модели пути каталога
+
+        Args:
+            path: (Path): объект Path с путем каталога
+            skip_check (bool): флаг пропуска проверки на существование. По-умолчание: False
+        """
         self._path: Path = path
 
         if not skip_check:
@@ -11,27 +26,62 @@ class PathModel:
 
     @property
     def files_count(self) -> int:
+        """
+        Количество файлов в каталоге
+
+        Returns:
+            (int): количество файлов в self._path
+
+        Examples:
+            >>> # Количество файлов в "C:/Users/admin/Downloads"
+            >>> PathModel(Path("C:/Users/admin/Downloads")).files_count
+        """
         file_count: int = 0
 
         for item in self._path.iterdir():
             if item.is_file():
                 file_count += 1
 
-        return  file_count
+        return file_count
 
     @property
     def files(self) -> Generator[Path, Any, None]:
+        """
+        Генератор списка файлов в каталоге
+
+        Returns:
+            (Generator[Path, Any, None]): генератор списка файлов в self._path
+        """
         for item in self._path.iterdir():
             if item.is_file():
-                yield  item
+                yield item
 
     def create(self) -> None:
+        """
+        Создание каталога
+
+        Notes:
+
+            * Создаст полную структуру
+            * Допускает существование
+
+        Examples:
+            >>> # Создаст все несуществующие папки до 3
+            >>> PathModel(Path("C:/1/2/3")).create()
+
+        """
         self._path.mkdir(parents=True, exist_ok=True)
 
-
     def _check_exists(self) -> None:
+        """
+        Проверяет существование каталога
+
+        Raises:
+            FileNotFoundError: если папка не существует
+            NotADirectoryError: если переданный путь не является каталогом
+        """
         if not self._path.exists():
-            raise FileExistsError(f"Папка {self._path} не существует")
+            raise FileNotFoundError(f"Папка {self._path} не существует")
 
         if not self._path.is_dir():
             raise NotADirectoryError(f"Путь {self._path} не папка")
